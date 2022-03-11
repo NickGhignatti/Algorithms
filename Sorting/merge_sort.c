a64c326c93e5914df2a866443b06dd143a766b98
Ghignatti Nicolò <94747281+NickGhignatti@users.noreply.github.com>
2022-03-11 18:04:05 +0100

    Update merge_sort.c

--- Sorting/merge_sort.c
+++ Sorting/merge_sort.c
@@ -1,36 +1,36 @@
-void merge(int *v, int start, int middle, int end, int *buffer) {
+void merge(int *arr, int start, int middle, int end, int *buffer) {
     int i = start, j = (middle + 1), k = 0;
     while (i <= middle && j <= end) {                         // looping until the 2 index will switch,
-        if (v[i] <= v[j]){                                    // every time gonna check who has the lower number 
-            buffer[k] = v[i];                                 // between the 2 part (or sub-array) and going to the
+        if (arr[i] <= arr[j]){                                    // every time gonna check who has the lower number 
+            buffer[k] = arr[i];                                 // between the 2 part (or sub-array) and going to the
             i++;                                              // next value of the onw with it,
         }
         else{
-            buffer[k] = v[j];
+            buffer[k] = arr[j];
             j++;
         }
         k++;                                                  // then increasing the position of the array storing the 
     }                                                         // ordered values.
     while (i <= middle){                                      // The extra value of one of the 2 parts (or sub-arrays)
-        buffer[k] = v[i];                                     // is gonna get inserted at the very least
+        buffer[k] = arr[i];                                     // is gonna get inserted at the very least
         i++;
         k++;
     }
     while (j <= r){
-        buffer[k] = v[j];
+        buffer[k] = arr[j];
         j++;
         k++;
     }
     for (k = start; k <= end; k++){                           // and then the array containing the ordered values is copied
-        v[k] = buffer[k - start];                             // in the array
+        arr[k] = buffer[k - start];                             // in the array
     }
 }
 
-void merge_sort(int *v, int start, int end, int *buffer) {
+void merge_sort(int *arr, int start, int end, int *buffer) {
     if (start < end){                                         // if the start index is less than the end
         int midlle = (start + end) / 2;                       // calculating the half
-        merge_sort(v, start, middle, buffer);                 // dividing, ordering and merging the left part
-        merge_sort(v, middle + 1, end, buffer);               // dividing, ordering and merging the right part
-        merge(v, start, middle, end, buffer);                 // mergin the 2 parts
+        merge_sort(arr, start, middle, buffer);                 // dividing, ordering and merging the left part
+        merge_sort(arr, middle + 1, end, buffer);               // dividing, ordering and merging the right part
+        merge(arr, start, middle, end, buffer);                 // mergin the 2 parts
     }
 }
