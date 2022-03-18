fc6399479f4b3e3a051f981f42cbe58211e52c87
Ghignatti Nicolò <94747281+NickGhignatti@users.noreply.github.com>
2022-03-18 09:15:58 +0100

    Update insertion_sort.c

--- Sorting/insertion_sort.c
+++ Sorting/insertion_sort.c
@@ -2,7 +2,7 @@ void insertion_sort(int *arr, int length) {
 	int i;
 	for (i = 1; i < length; i++) {                         // get every item of the array except for the first one 
 		int value = arr[i];                            // and assign it as the value to sort in the array, flowing them another
-		int j = (i - 1);                               // time (from the beginning this time) to see if there is a lower value
+		int j = (i - 1);                               // time (from the beginning this time)
 		while (j >= 0 && arr[j] > value) {             // looking for an higher value
 			arr[j + 1] = arr[j];                   // if it has found it, gonna switch and moving to the next index
 			j--;
